5866caecdbea4da26d16e0f174ab24b836fdb050
Drevo23 <jan.rojicek2005@gmail.com>
2024-05-31 16:56:36 +0200

    Card creator DEMO
    WORKING

--- ProjektWEA/source/commons/cardCreator.php
+++ ProjektWEA/source/commons/cardCreator.php
@@ -6,9 +6,17 @@
         <meta name="viewport" content="width=device-width,initial-scale=1.0">
         <title>Home</title>
         <link rel="stylesheet" href="../assets/index.css">
+        <link rel="stylesheet" href="../assets/cardstyle.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     </head>
     <body>
+    <style>
+      
+      canvas{
+        background-color: red;
+      }
+     
+    </style>
         <div class="hero">
             <nav>
                 <a class="logo" href="index.html">Fames</a>
@@ -52,17 +60,30 @@
 
             </nav>
 
-            <div class="home">
-                <div class="intro">
-                    <h1>WELCOME TO FAMES</h1>
-                    <p>Tu muzem napsat nejaky text voleee</p>
-                </div>   
-                
+            <div class="home">        
                 <!-----------------------------------
                 *      Zde si muzes vlozit ty       *
                 *      herni karty podle sebe       *                                   
                 ------------------------------------>
+                <div class="card">  
+              <input class="title" type="text">
+              <div class="like"></div>
+              <div class="gem"></div><canvas id="myCanvas" width="720" height="1050"></canvas>
+              <textarea class="text"></textarea>
+              <p class="iden1">Cardset: </p>
+             <p class="iden2">user: </p>
+              
+
+               </div>
                 
+                <a id="download" download="img-bozi.jpeg" href="#">stahni</a>
+                <input id="obrazek" type="file" accept="image/png, image/jpeg" />
+                <input type="range"  max="0" value="0" class="slider-square" id="x">
+                <input type="range"  max="0" value="0" class="slider-square" id="y">
+
+
+
+
             </div>
         </div>
         
@@ -74,7 +95,57 @@
             }
 
         </script>
+        
+        <script>
+        const canvas = document.getElementById('myCanvas');
+        const obrazek = document.getElementById('obrazek');
+        var context = canvas.getContext('2d');
+        var x=0,y=0;
+      var imageObj = new Image();
+      var source;
+      obrazek.addEventListener("change", function () {
+       getImgData();
+       context.drawImage(imageObj,0,0);      
+            });
+      
 
+            function getImgData() {
+  const files = obrazek.files[0];
+  if (files) {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(files);
+    fileReader.addEventListener("load", function () {
+
+      source=this.result;
+      imageObj.src = source;
+    });    
+  }
+}
+      imageObj.onload = function() {
+          context.drawImage(imageObj,0,0);
+          var data = canvas.toDataURL("image/jpeg");
+          document.getElementById("download").href =data;
+          console.log(imageObj.naturalWidth);
+       console.log(imageObj.naturalHeight);
+       document.getElementById('x').setAttribute("min",-imageObj.naturalWidth+720);
+       document.getElementById('y').setAttribute("min",-imageObj.naturalHeight+1050);
+          
+  }; 
+ 
+  document.getElementById("x").oninput = function() {
+    var data = canvas.toDataURL("image/jpeg");
+    document.getElementById("download").href =data;
+    x=this.value;
+   context.drawImage(imageObj,x,y);
+  }
+  document.getElementById("y").oninput = function() {
+    var data = canvas.toDataURL("image/jpeg");
+    document.getElementById("download").href =data;
+    y=this.value;
+   context.drawImage(imageObj,x,y);
+  }
+
+      </script>
         <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
         <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     </body>
